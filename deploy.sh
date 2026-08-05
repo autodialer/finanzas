001232f20b151f5b296a328c87a19cfefdead310
autodialer <autodialer@gmail.com>
2026-08-05 08:37:08 -0600

    chore: correr migraciones automáticamente en cada deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ set -e
 cd /home/ccarlos/finanzas
 echo '🔄 Jalando cambios de GitHub...'
 git pull origin main
+echo '🗄️  Corriendo migraciones...'
+php artisan migrate --force
 echo '🧹 Limpiando cache...'
 php artisan cache:clear
 php artisan config:clear
